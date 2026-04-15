b42f4d9e981c15ea005f9e33b95af40ca9c6d03f
Khushbu Rauthan <khushburauthan8@gmail.com>
2026-04-15 11:49:43 +0530

    Add files via upload
    Python programming projects including To-Do List, Calculator and Password Generator.

---
+++ todo.py
@@ -0,0 +1,50 @@
+tasks = []
+while True:
+    print("TO-DO LIST")
+    print("1- Add Task")
+    print("2- View Tasks")
+    print("3- Update Task")
+    print("4- Delete Task")
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        task = input("Enter task: ")
+        tasks.append(task)
+        print("Task added.")
+
+    elif choice == "2":
+        if len(tasks) == 0:
+            print("No tasks.")
+        else:
+            print("\nYour Tasks:")
+            for i in range(len(tasks)):
+                print(i + 1, "-", tasks[i])
+
+    elif choice == "3":
+        if len(tasks) == 0:
+            print("No tasks to update.")
+        else:
+            for i in range(len(tasks)):
+                print(i + 1, "-", tasks[i])
+            num = int(input("Enter task number to update:"))
+            if 1 <= num <= len(tasks):
+                new_task = input("Enter new task:")
+                tasks[num - 1] = new_task
+                print("Task updated.")
+            else:
+                print("Invalid number.")
+
+    elif choice == "4":
+        if len(tasks) == 0:
+            print("No tasks to delete.")
+        else:
+            for i in range(len(tasks)):
+                print(i + 1, "-", tasks[i])
+            num = int(input("Enter task number to delete."))
+            if 1 <= num <= len(tasks):
+                removed = tasks.pop(num - 1)
+                print("Deleted task:", removed)
+            else:
+                print("Invalid number.")
+    else:
+        print("Invalid choice.")
